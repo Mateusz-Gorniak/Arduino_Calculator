3c5897fb4012b846c752ee2cdd223c71f9eb951f
Mateusz <emias4@interia.pl>
2020-07-07 22:16:16 +0200

    add3

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,23 @@
 #include <LiquidCrystal.h>
 #include <stdio.h>
 
-LiquidCrystal lcd(12,11,5,4,3,2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 char rxBuff[32];
 int ValueA = 0, ValueB = 0;
 char operation[16];
 
-void setup(){
-lcd.begin(16, 2);
-lcd.print("Nazwisko");
-
-Serial.begin(9600);
-Serial.setTimeout(3000);
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.print("Nazwisko");
+  Serial.begin(9000);
+  Serial.setTimeout(6000);
 }
 
-void loop(){
-  Serial.readBytesUntil('\r', rxBuff, sizeof(rxBuff)-1);
-  lcd.setCursor(0,0);
+void loop()
+{
+  Serial.readBytesUntil('\r', rxBuff, sizeof(rxBuff) - 1);
+  lcd.setCursor(0, 0);
   lcd.print(rxBuff);
 }
